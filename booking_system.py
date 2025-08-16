2c8a82dd032c18cba3dd31f5893088d1f2e318a0
Sandesh Salunke <SANDESHSALUNKE55@GMAIL.COM>
2025-08-16 18:22:36 +0530

    reject overlapping slots

--- booking_system.py
+++ booking_system.py
@@ -6,9 +6,13 @@ class TurfBooking:
         self.slot_minutes = slot_minutes
         self.booked_slots = []
 
-    def book_slot(self, start: datetime):
-        end = start + timedelta(minutes=self.slot_minutes)
-        self.booked_slots.append((start, end))
+    def book_slot(self, requested_start: datetime):
+    
+        requested_end = requested_start + timedelta(minutes=self.slot_minutes)
+        for start, end in self.booked_slots:
+            if start < requested_end and requested_start < end:
+                return False
+        self.booked_slots.append((requested_start, requested_end))
         return True
 
     def get_bookings(self):
