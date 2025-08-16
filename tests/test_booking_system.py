c9ec1b2fc35b003db9555a076206c42da788765e
Sandesh Salunke <SANDESHSALUNKE55@GMAIL.COM>
2025-08-16 18:34:19 +0530

    test case for midnight booking

--- tests/test_booking_system.py
+++ tests/test_booking_system.py
@@ -29,3 +29,11 @@ def test_booking_in_past():
     turf = TurfBooking(slot_minutes=30)
     past_time = datetime(2024, 12, 31, 23, 0)
     assert turf.book_slot(past_time) is False
+
+
+@freeze_time("2025-01-01 06:00:00")
+def test_booking_cross_midnight():
+    turf = TurfBooking(slot_minutes=30)
+    late = datetime(2025, 1, 1, 23, 30)
+    assert turf.book_slot(late) is True
+    assert turf.get_bookings()[0][1] == datetime(2025, 1, 2, 0, 0)
