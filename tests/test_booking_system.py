e7690c5499ab7d14ced40bd6e30c40e35f2a869e
Sandesh Salunke <SANDESHSALUNKE55@GMAIL.COM>
2025-08-16 18:30:38 +0530

    use freeze time for time validations

--- tests/test_booking_system.py
+++ tests/test_booking_system.py
@@ -3,16 +3,19 @@ import sys, os
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from booking_system import TurfBooking
+from freezegun import freeze_time
 
 
 from datetime import datetime, timedelta
 
+
+@freeze_time("2025-01-01 06:00:00")
 def test_book_first_slot():
     turf = TurfBooking(slot_minutes=30)
     start = datetime(2025, 1, 1, 7, 0)
     assert turf.book_slot(start) is True
 
-
+@freeze_time("2025-01-01 06:00:00")
 def test_overlapping_slot_not_allowed():
     turf = TurfBooking(slot_minutes=30)
     first = datetime(2025, 1, 1, 7, 0)
@@ -21,6 +24,7 @@ def test_overlapping_slot_not_allowed():
     overlap = datetime(2025, 1, 1, 7, 15)
     assert turf.book_slot(overlap) is False
 
+@freeze_time("2025-01-01 06:00:00")
 def test_booking_in_past():
     turf = TurfBooking(slot_minutes=30)
     past_time = datetime(2024, 12, 31, 23, 0)
